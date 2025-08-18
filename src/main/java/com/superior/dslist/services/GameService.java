fcaeb66b3e059176342da0750da1a54b5df976ef
IDP27 <iuridp27@gmail.com>
2025-08-18 16:39:57 -0300

    GameRepository, service, controller

---
+++ src/main/java/com/superior/dslist/services/GameService.java
@@ -0,0 +1,32 @@
+package com.superior.dslist.services;
+
+
+
+import com.superior.dslist.Dto.GameMinDTO;
+import com.superior.dslist.entities.Game;
+import com.superior.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll() {
+
+        List <Game> result = gameRepository.findAll();
+        List <GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+
+        return dto;
+
+
+
+
+
+}
+}
+
