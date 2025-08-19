96d382054d28e204fe0bd85c7d9737a8bbf824e6
IDP27 <iuridp27@gmail.com>
2025-08-19 11:27:45 -0300

    GET Lists

---
+++ src/main/java/com/superior/dslist/controllers/GameListController.java
@@ -0,0 +1,29 @@
+package com.superior.dslist.controllers;
+
+
+import com.superior.dslist.Dto.GameListDTO;
+import com.superior.dslist.services.GameListService;
+import com.superior.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+
+    @Autowired
+    private GameListService gameListService;
+
+    @GetMapping
+    public List <GameListDTO>findAll(){
+        List <GameListDTO> result = gameListService.findAll();
+        return result;
+
+    }
+
+
+}
---
+++ src/main/java/com/superior/dslist/repositories/GameListRepository.java
@@ -0,0 +1,10 @@
+package com.superior.dslist.repositories;
+
+import com.superior.dslist.entities.GameList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameListRepository extends JpaRepository <GameList, Long> {
+
+    // Aqui você pode adicionar métodos personalizados se necessário
+    // Exemplo: List<GameList> findByNameContaining(String name);
+}
