21f9b92927cca90b82c3b638a0bbe4230031d55a
IDP27 <iuridp27@gmail.com>
2025-08-19 10:52:29 -0300

    GET game by id

--- src/main/java/com/superior/dslist/services/GameService.java
+++ src/main/java/com/superior/dslist/services/GameService.java
@@ -2,11 +2,13 @@ package com.superior.dslist.services;
 
 
 
+import com.superior.dslist.Dto.GameDTO;
 import com.superior.dslist.Dto.GameMinDTO;
 import com.superior.dslist.entities.Game;
 import com.superior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -20,6 +22,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true) // Indica que o metodo nao ira modificar o banco de dados, apenas ler
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
 
         List <Game> result = gameRepository.findAll();
