6db18174f97574cbd22bb34b3bf0c5bb3c6510f0
IDP27 <iuridp27@gmail.com>
2025-08-20 12:29:48 -0300

    Homologacao - ok

--- src/main/java/com/superior/dslist/Dto/GameMinDTO.java
+++ src/main/java/com/superior/dslist/Dto/GameMinDTO.java
@@ -25,7 +25,7 @@ public class GameMinDTO { // Classe DTO (Data Transfer Object) para transferir d
     public GameMinDTO (GameMinProjection projection) {
         id = projection.getId();
         title = projection.getTitle();
-        year = projection.getYear();
+        year = projection.getGameYear();
         imgUrl = projection.getImgUrl();
         shortDescription = projection.getShortDescription();
     }
